87d479412137bdeb30a9442764b4c76fa132874d
kaelonnakaler-create <kaelonna.kaler@students.msswarriors.org>
2025-12-17 19:28:07 +0000

    check

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var sum = 1 + 4;
-var difference = 3;
-var product = 10;
-var quotient = 2.5;
-var remainder = 2;
+var difference = (3);
+var product = 15;
+var quotient = (2);
+var remainder = (2);
 
-document.getElementById("result").innerHTML = [{quotient}];
+document.getElementById("result").innerHTML = (${quotient};);
