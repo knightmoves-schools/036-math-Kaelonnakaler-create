99833e9fc499ffb39c15409b9625df9c999ce09c
kaelonnakaler-create <kaelonna.kaler@students.msswarriors.org>
2025-12-17 19:23:00 +0000

    try this

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var sum = 1 + 4;
-var difference = 2;
-var product = 5;
-var quotient = 2;
+var difference = 3;
+var product = 10;
+var quotient = 2.5;
 var remainder = 2;
 
-document.getElementById("result").innerHTML = total;
+document.getElementById("result").innerHTML = [{quotient}];
