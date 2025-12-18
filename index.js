e8451a3c4faf4d092fc590b86c1e85f244f1bdea
kaelonnakaler-create <kaelonna.kaler@students.msswarriors.org>
2025-12-18 16:13:06 +0000

    try this

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var sum = 1 + 4;
 var difference = 3;
 var product = 15;
-var quotient = 2;
-var remainder = 2;
+var quotient = 7.5;
+var remainder = 1;
 
-document.getElementById("result").innerHTML = ($quotient);
+document.getElementById("result").innerHTML = ($2);
